b3e8a3ec31396bff24e15973fec7984ce90636fe
Khalid Elshafie <abolkog@gmail.com>
2017-12-09 00:52:56 +0900

    Adding Chat UI

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,119 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, FlatList, 
+    Platform, Keyboard,
+    TouchableHighlight, KeyboardAvoidingView } from 'react-native';
+import { Header } from 'react-native-elements';
+
+import ChatItem from './ChatItem';
 
 // create a component
 class Chat extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: '',
+            disabled: true,
+            messages: [
+                {
+                    id: 1,
+                    text: 'Hello',
+                    avatar: 'http://abolkog.com/img/default_user.png',
+                    author: {
+                        id: 1,
+                        avatar: 'http://abolkog.com/img/default_user.png',
+                        username: 'Khalid Tany Khals'
+                    }
+                },
+                {
+                    id: 2,
+                    text: 'How are you',
+                    author: {
+                        id: 2,
+                        avatar: 'http://abolkog.com/img/default_user.png',
+                        username: 'Khalid'
+                    }
+                    
+                }
+            ]
+        }
+    }
+
+    onTyping(text) {
+       if (text && text.length >= 2 ) {
+           this.setState({
+               disabled: false,
+               text
+           });
+       } else {
+           this.setState({
+               disabled: true
+           })
+       }
+    }
+
+    onSendBtnPressed () {
+        const messages = this.state.messages;
+        const newMessage = {
+            text: this.state.text,
+            author: {
+                id: 1,
+                avatar: 'http://abolkog.com/img/default_user.png',
+                username: 'Khalid'
+            }
+        };
+
+        messages.unshift(newMessage);
+        this.setState({ messages })
+        this.textInput.clear();
+        Keyboard.dismiss();
+    }
+
+    renderChatItem({ item }) {
+        return <ChatItem message={item} />
+    }
+
+    keyExtractor = (item, index) => index;
+
     render() {
+        const extraBtnStyle = this.state.disabled ? styles.disabledBtn : styles.enabledBtn;
+        let behavior = '';
+        if (Platform.OS == 'ios')  {
+            behavior = 'padding'
+        }
         return (
             <View style={styles.container}>
-                <Text>Chat</Text>
+                <Header
+                    centerComponent={{ text: 'Wanasa', style: { color: '#fff', fontSize: 20 } }}
+                />
+
+                <FlatList 
+                    inverted
+                    data={this.state.messages}
+                    renderItem={this.renderChatItem}
+                    keyExtractor={this.keyExtractor}
+                />
+
+                <KeyboardAvoidingView behavior={behavior}>
+                    <View style={styles.inputBar}>
+                        
+                        <TextInput 
+                            style={styles.textBox} 
+                            multiline
+                            onChangeText={(text) => this.onTyping(text)}
+                            ref={input => { this.textInput = input; } }
+                        />
+
+                        <TouchableHighlight 
+                            style={[styles.sendBtn, extraBtnStyle ]}
+                            disabled={this.state.disabled}
+                            onPress={this.onSendBtnPressed.bind(this)}
+                        >
+                            <Text style={{ color: '#fff'}}>Send</Text>
+                        </TouchableHighlight>
+                    </View>
+                </KeyboardAvoidingView>
+                
             </View>
         );
     }
@@ -16,11 +122,39 @@ class Chat extends Component {
 // define your styles
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    inputBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHoriztonal: 5,
+        paddingVertical: 10,
+        backgroundColor: '#dadfea'
+    },
+    textBox: {
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        fontSize: 14,
+        paddingHoriztonal: 10,
         flex: 1,
+        paddingVertical: 5,
+        marginLeft: 5
+    },
+    sendBtn: {
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5,
+        marginLeft: 5
     },
+    enabledBtn: {
+        backgroundColor: '#476DC5'
+    },
+    disabledBtn: {
+        backgroundColor: '#89a9f4'
+    }
 });
 
 //make this component available to the app
---
+++ src/components/ChatItem.js
@@ -0,0 +1,71 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Avatar } from 'react-native-elements';
+
+// create a component
+class ChatItem extends Component {
+
+    showAvatarOrNoT(message) {
+        if (message.author.id !== 2 ) {
+            return (
+                <Avatar
+                    source={{ uri: message.author.avatar }}
+                    small
+                    rounded
+                />
+            );
+        }
+
+        return <View/>
+    }
+    render() {
+        const message = this.props.message;
+        const isMyMessage = message.author.id == 2;
+        const textContainerExtra = isMyMessage ? styles.textContainerRight : styles.textContainerLeft;
+        return (
+            <View style={styles.messageContainer}>
+                {this.showAvatarOrNoT(message)}
+                <View style={[styles.textContainer, textContainerExtra ]}>
+                    <Text style={styles.sender}>{message.author.username}</Text>
+                    <Text style={styles.message}>{message.text}</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+    messageContainer: {
+        flexDirection: 'row',
+        padding: 20
+    },
+    textContainer: {
+        flexDirection: 'column',
+        marginLeft: 10,
+        marginRight: 10,
+        flex: 1,
+        borderRadius: 5,
+        paddingHorizonal: 10,
+        paddingVertical: 5
+    },
+    textContainerLeft:{
+        alignItems: 'flex-start',
+        backgroundColor: '#d5d8d4'
+    },
+    textContainerRight: {
+        alignItems: 'flex-end',
+        backgroundColor: '#66db30'
+    },
+    message: {
+        fontSize: 16
+    },
+    sender: {
+        fontWeight: 'bold',
+        paddingRight: 10
+    }
+});
+
+//make this component available to the app
+export default ChatItem;
